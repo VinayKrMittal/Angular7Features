7249fcbbeffe8584580c135990d9617f65645c80
vinaymittal <vinaymittal1996@gmail.com>
2018-12-28 22:05:15 +0530

    add nav bar as parent route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 import { VirtualScrollComponent } from './virtual-scroll/virtual-scroll.component';
 import { DragDropComponent } from './drag-drop/drag-drop.component';
 import { HomeComponent } from './home/home.component';
-const routes: Routes = [
-  { path: 'virtual-scroll', component: VirtualScrollComponent },
-  { path: 'drag-drop', component: DragDropComponent },
-  { path: 'home', component: HomeComponent },
-  { path: '**', component: HomeComponent }
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-];
+const routes: Routes = [
+  {
+    path: '', component: NavBarComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'virtual-scroll', component: VirtualScrollComponent },
+      { path: 'drag-drop', component: DragDropComponent },
+      { path: 'home', component: HomeComponent },
+      { path: '**', component: HomeComponent }
+    ]
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
